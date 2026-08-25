f2f2b13b44e31af67451e44dd53f09489524843c
Amos Kiplangat <amoscheruiyot14024@gmail.com>
2026-08-25 23:42:09 +0300

    chore: update config with environment variable support

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///bookscout.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default-dev-key')
+    
